f02065c072b447972145d10752e8cd39339dcc8a
liangguohun <liangguohun@qq.com>
2018-01-14 23:49:31 +0800

    东东

---
+++ springCloud_solr/src/main/java/com/homefun/SpringCloudSolrApplication.java
@@ -0,0 +1,12 @@
+package com.homefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSolrApplication.class, args);
+	}
+}
